0a8a8dade1e5de8de25b95de86712b349b038ccd
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-05-11 14:47:36 -0400

    se agrega request

--- core/Router.php
+++ core/Router.php
@@ -1,18 +1,33 @@
 <?php
+
 namespace app\core;
 
 class Router
 {
-    protected array $routes=[];
+    public Request $request;
+    protected array $routes = [];
 
-    public function get($path, $callback){
-        $this->routes['get'][$path]= $callback;
+    public function __construct(\app\core\Request $request)
+    {
+        $this->request = $request;
     }
 
-    public function resolve(){
-        echo '<pre>';
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        /*echo '<pre>';
         var_dump($_SERVER);
-        echo '/<pre>';
-        exit;
+        echo '</pre>';
+        exit;*/
+
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+
+        var_dump($path);
+        var_dump($method);
     }
 }
