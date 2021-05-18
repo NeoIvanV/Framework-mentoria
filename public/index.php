a68e697662d21d1676b18e60838c08a0beaf66c3
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-05-18 11:05:21 -0400

    se agrega vista

--- public/index.php
+++ public/index.php
@@ -4,12 +4,10 @@ require_once __DIR__. '/../vendor/autoload.php';
 use app\core\Application;
 $app = new Application();
 
-$app->router->get('/', function(){
-    return "Hola Ivan Vejar";
+$app->router->get('/', function () {
+    return "Hola Mundo";
 });
 
-$app->router->get('/contact', function(){
-    return "Contact";
-});
+$app->router->get('/contact', 'contact');
 
 $app->run();
