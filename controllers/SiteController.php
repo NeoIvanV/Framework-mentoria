414fbe82d15a5af70a83fe21fb80b0e5eb298556
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-05-27 11:45:58 -0400

    se agrega controller

---
+++ controllers/SiteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        return $this->render('home');
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
+    public function handleContact()
+    {
+        return "Procesando informacion";
+    }
+}
