10fa8937f8001184d8795eccbac372592d9c064c
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-06-01 10:23:06 -0400

    se agrega authcontroller

---
+++ views/layouts/contact.php
@@ -0,0 +1,16 @@
+<h1>Contact!!!</h1>
+<form method="POST">
+  <div class="mb-3">
+    <label class="form-label">Subject</label>
+    <input type="text" name="subject" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Email</label>
+    <input type="email" name="email" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Body</label>
+    <textarea name="body" class="form-control" id="" cols="30" rows="10"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Send</button>
+</form>
