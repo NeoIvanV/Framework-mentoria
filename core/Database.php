21957ab792e8baadfabe0c1af7a0d5a9494fc4e5
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-06-17 15:43:53 -0400

    se incoporpora migrations

--- core/Database.php
+++ core/Database.php
@@ -18,6 +18,37 @@ class Database
 
     public function applyMigrations()
     {
-        echo "Running applyMigrations\n";
+        $this->createMigrationsTable();
+        $appliedMigrations = $this->getAppliedMigrations();
+
+        $files = scandir(Application::$ROOT_DIR . '/migrations');
+        $toApplyMigrations = array_diff($files, $appliedMigrations);
+
+        foreach ($toApplyMigrations as $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+            require_once Application::$ROOT_DIR . '/migrations/' . $migration;
+            $className = pathinfo($migration, PATHINFO_FILENAME);
+            $instance = new $className();
+            echo "Applying migration $migration\n";
+            $instance->up();
+            echo "Applied migration $migration\n";
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS `migrations` ( `id` INT NOT NULL AUTO_INCREMENT , `migration` VARCHAR(255) NOT NULL , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB; ");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $sql = "SELECT migration FROM migrations";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
     }
 }
